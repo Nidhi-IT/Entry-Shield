0e1cd3941a6df8ff5057f64c3372794f4f8907cc
nidhi <nidhisigh013@gmail.com>
2026-03-29 19:43:07 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -2,25 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from "@tailwindcss/vite";
 
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/setupTests.js'],
-    globals: true,
-    coverage: {
-      provider: 'v8',
-      reporter: ['text', 'html'],
-      thresholds: { lines: 90, functions: 90, branches: 90, statements: 90 },
-      exclude: [
-        'node_modules/',
-        'src/setupTests.js',
-        'src/main.jsx',      // <-- Exclude this!
-        'src/index.css',     // <-- Exclude CSS
-        '**/*.d.ts',
-        'eslint.config.js',
-        'vite.config.js'
-      ]
-    }
-  }
+  plugins: [react(),tailwindcss()],
 })
